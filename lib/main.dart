fc4e60ed564aae978b280a5280131e7ee6700b07
bishalkhatiwada <bishal.khatiwada.sibs.edu@gmail.com>
2023-05-16 13:47:17 +0545

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:riderapp/screens/home_page.dart';
+import 'package:riderapp/screens/splash_screen.dart';
+import 'package:sizer/sizer.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Sizer(
+      builder: (context, orientation, deviceType) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Sizer',
+          theme: ThemeData.light(),
+          home: SplashScreen(),
+        );
+      },
+    );
+  }
+}
---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:riderapp/screens/home_page.dart';
+import 'package:sizer/sizer.dart';
+
+import 'login_page.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashState();
+}
+
+class _SplashState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => const HomePage()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Stack(alignment: Alignment.center, children: [
+          Image.asset(
+            'assets/bike.webp',
+            fit: BoxFit.cover,
+            height: double.infinity,
+            width: double.infinity,
+          ),
+          if (defaultTargetPlatform == TargetPlatform.iOS)
+            CupertinoActivityIndicator(
+              color: Colors.white,
+              radius: 7.w,
+            )
+          else
+            const CircularProgressIndicator(
+              color: Colors.white,
+            ),
+        ]),
+      ),
+    );
+  }
+}
